da97402d1361457521029df3d7e8cc5bd4f6359c
fkonfederak <fkonfederak.fk@gmail.com>
2025-10-21 08:00:23 +0000

    Com2_1.4

--- script.js
+++ script.js
@@ -27,28 +27,46 @@
           });
           html += `<div>Total: ${array.length}</div>`;
           answer.innerHTML = html;
-            fetch('https://jsonplaceholder.typicode.com/posts/1')
+          fetch('https://jsonplaceholder.typicode.com/posts/1')
+            .then(response => response.json())
+            .then(post => {
+              answer.innerHTML += `<div>${post.title}</div>`;
+            })
+          setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+              method: 'POST',
+              body: JSON.stringify({
+                title: 'New post',
+                body: 'Text',
+                userId: 1,
+              }),
+              headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+            })
               .then(response => response.json())
-              .then(post => {
-                answer.innerHTML += `<div>${post.title}</div>`;
+              .then(data => {
+                answer.innerHTML = `<p> Dodano nowy post o ID = ${data.id}</p>`;
               })
-          .catch(error => {
-            console.error('Error:', error);
-          })
-      }, 2000);
+          }, 5000)
+
+            .catch(error => {
+              console.error('Error:', error);
+            })
+        }, 2000);
         return array;
-  })
-    .catch(error => {
-      console.error('Error:', error);
-    })
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
   });
 
-cw2.addEventListener("click", function() {
-  //TODO
-})
+  cw2.addEventListener("click", function() {
+    //TODO
+  })
 
-cw3.addEventListener("click", function() {
-  //TODO
-})
+  cw3.addEventListener("click", function() {
+    //TODO
+  })
 
-}) ();
+})();
