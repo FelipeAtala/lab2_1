dc291a4ae736abe9a4fec2fad346da53957139c6
fkonfederak <fkonfederak.fk@gmail.com>
2025-10-21 07:48:58 +0000

    Com2_1.3

--- script.js
+++ script.js
@@ -20,26 +20,35 @@
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
-        setTimeout(() => { 
-        let html = '';
-        array.forEach(function(item) {
-          html += `<div>${item.title}</div>`;
-        });
-        html += `<div>Total: ${array.length}</div>`;
-        answer.innerHTML = html;
-        }, 2000);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      })
+        setTimeout(() => {
+          let html = '';
+          array.forEach(function(item) {
+            html += `<div>${item.title}</div>`;
+          });
+          html += `<div>Total: ${array.length}</div>`;
+          answer.innerHTML = html;
+            fetch('https://jsonplaceholder.typicode.com/posts/1')
+              .then(response => response.json())
+              .then(post => {
+                answer.innerHTML += `<div>${post.title}</div>`;
+              })
+          .catch(error => {
+            console.error('Error:', error);
+          })
+      }, 2000);
+        return array;
   })
+    .catch(error => {
+      console.error('Error:', error);
+    })
+  });
 
-  cw2.addEventListener("click", function() {
-    //TODO
-  })
+cw2.addEventListener("click", function() {
+  //TODO
+})
 
-  cw3.addEventListener("click", function() {
-    //TODO
-  })
+cw3.addEventListener("click", function() {
+  //TODO
+})
 
-})();
+}) ();
